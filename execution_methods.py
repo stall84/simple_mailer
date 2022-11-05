2953dd0fb26bafb985c64976d615d827d0da1268
Michael Stallings <stall84@gmail.com>
2022-11-05 11:25:03 -0400

    impromptu tutorial from realpython.com on execution contexts main functions, etc

---
+++ execution_methods.py
@@ -0,0 +1,5 @@
+print("This is my file to test Python's execution methods.")
+print("The variable __name__ tells me which context this file is running in.")
+print("The value of __name__ is:", repr(__name__))
+print("The value of __doc__ is:", repr(__doc__))
+print("The value of __package__ is:", repr(__package__))
---
+++ main.py
@@ -0,0 +1,5 @@
+from simple_mailer import SimpleMailer
+
+
+def main():
+    pass
